eacfa1f8c4b11bfc101d4c64a6e0d393429044ea
Myawwaa <murarikovajulia@gmail.com>
2026-07-08 13:47:41 +0200

    Add files via upload

---
+++ 01_prvni_program.py
@@ -0,0 +1,19 @@
+jmeno = "jula"
+mesto = 'kudrnabrno'
+zvire = 'kocour'
+jidlo = zvire
+hra = 'gi'
+print ('jmeno:')
+print (jmeno)
+print ('')
+print ('mesto:')
+print (mesto)
+print ('')
+print ('oblibene jidlo:')
+print (jidlo)
+print ('')
+print ("oblibene zvire:")
+print(jidlo)
+print('')
+print('oblibena hra:')
+print(hra)
